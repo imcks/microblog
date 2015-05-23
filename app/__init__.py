fedeb04e6853c6df8457c6d0497eb3dd0eeebdf0
Shan <kais.c@outlook.com>
2015-05-23 17:56:07 +0800

    use login,Profile page,Unit Testing until bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,41 @@
+#coding=utf-8
+import os
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# ps:下面是在非dubug模式下开启邮件功能
+"""
+在没有邮件服务器的pc上测试邮件功能也很容易，Python有SMTP的测试排错的服务器(SMTP debugging server).
+打开一个控制台窗口，并且运行下面的命令:
+python -m smtpd -n -c DebuggingServer localhost:25
+"""
+if not app.debug:
+	import logging
+	from logging.handlers import SMTPHandler
+	credentials = None
+	if MAIL_USERNAME or MAIL_PASSWORD:
+		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+	mail_handler.setLevel(logging.ERROR)
+	app.logger.addHandler(mail_handler)
+# 开启文件日志
+if not app.debug:
+	import logging
+	from logging.handlers import RotatingFileHandler
+	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s:%(message)s [in %(pathname)s:%(lineno)d]'))
+	app.logger.setLevel(logging.INFO)
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.info('microblog startup')
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
